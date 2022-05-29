7f229e065c60209079d9869460952dd9f19cf130
Maggie Saldivia <msaldivia@msaldivia-macbookpro.local>
2022-05-29 15:25:41 -0400

    setting up mongodb - problem with .env file

---
+++ controllers/loginController.js
@@ -0,0 +1,19 @@
+
+
+const registerView = (req, res) => {
+  res.render("register", {
+
+  })
+}
+
+const loginView  = (req, res) => {
+
+  res.render("login", {
+
+  })
+}
+
+module.exports = {
+  registerView,
+  loginView
+}
--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 //js
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config({ path: './sample.env' });
+
+//mongo
+const database = process.env.MONGLAB_URI;
+mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => console.log("db connected"))
+.catch(err => console.log(err))
+
+app.set('view engine', 'ejs')
+
+//routes
+app.use('/', require('./routes/login'));
 const PORT = process.env.PORT || 4111;
-app.listen(PORT, console.log("Server don start for port: " + PORT))
+app.listen(PORT, console.log("Server don start for port: " + PORT + database))
